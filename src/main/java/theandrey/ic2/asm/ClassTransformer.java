ac12c2e7eb8d72093e2ea8b07af5f26538610552
Andrey <andrey9506@yandex.ru>
2018-02-06 02:19:14 +0200

    Добавлена поддержка Advanced Machines

--- src/main/java/theandrey/ic2/asm/ClassTransformer.java
+++ src/main/java/theandrey/ic2/asm/ClassTransformer.java
@@ -1,10 +1,12 @@
 package theandrey.ic2.asm;
 
 import cpw.mods.fml.relauncher.FMLRelaunchLog;
+import java.util.ListIterator;
 import net.minecraft.launchwrapper.IClassTransformer;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.InsnList;
 import org.objectweb.asm.tree.MethodInsnNode;
@@ -18,14 +20,14 @@ public final class ClassTransformer implements IClassTransformer {
 
 	@Override
 	public byte[] transform(String origName, String name, byte[] bytes) {
-		if(name.equals("ic2.core.block.machine.tileentity.TileEntityRecycler")) {
+		if(name.equals("ic2.core.block.machine.tileentity.TileEntityRecycler") || name.equals("ic2.advancedmachines.TileEntityAdvancedRecycler")) {
 			ClassNode node = readClass(bytes);
 			processRecycler(node);
-			return writeClass(node);
+			bytes = writeClass(node);
 		} else if(name.equals("ic2.core.block.machine.tileentity.TileEntityRecycler$RecyclerRecipeManager")) {
 			ClassNode node = readClass(bytes);
 			processRecyclerRecipeManager(node);
-			return writeClass(node);
+			bytes = writeClass(node);
 		}
 		return bytes;
 	}
@@ -33,11 +35,23 @@ public final class ClassTransformer implements IClassTransformer {
 	private void processRecycler(ClassNode node) {
 		for(MethodNode method : node.methods) {
 			if(method.name.equals("operateOnce")) {
-				InsnList list = new InsnList();
-				list.add(new VarInsnNode(Opcodes.ALOAD, 0)); // this
-				list.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "theandrey/ic2/RecyclerStatMod", "hook_operateOnce", "(Lnet/minecraft/tileentity/TileEntity;)V", false));
-				method.instructions.insert(list);
-				FMLRelaunchLog.info("[RecyclerStat] Method TileEntityRecycler.operateOnce() hooked.");
+
+				ListIterator<AbstractInsnNode> it = method.instructions.iterator();
+				while(it.hasNext()) {
+					AbstractInsnNode insn = it.next();
+					if(insn.getOpcode() == Opcodes.INVOKEVIRTUAL) {
+						MethodInsnNode mcall = (MethodInsnNode)insn;
+						if(mcall.name.equals("consume") && mcall.owner.equals("ic2/core/block/invslot/InvSlotProcessable")) {
+
+							InsnList insert = new InsnList();
+							insert.add(new VarInsnNode(Opcodes.ALOAD, 0)); // this
+							insert.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "theandrey/ic2/RecyclerStatMod", "hook_operateOnce", "(Lnet/minecraft/tileentity/TileEntity;)V", false));
+							method.instructions.insert(insn, insert);
+							FMLRelaunchLog.info("[RecyclerStat] Method TileEntityRecycler.operateOnce() hooked.");
+							break;
+						}
+					}
+				}
 				break;
 			}
 		}
